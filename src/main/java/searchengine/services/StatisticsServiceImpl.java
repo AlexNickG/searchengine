7135ecd593b3daf179bc2a265aa6080d9df1fbbc
Aleksey Gorshkov <angorshkov@yandex.ru>
2025-02-08 23:33:40 +0300

    Status time corresponding correctly pom.xml edited for deploy readme.md version 2

--- src/main/java/searchengine/services/StatisticsServiceImpl.java
+++ src/main/java/searchengine/services/StatisticsServiceImpl.java
@@ -7,26 +7,24 @@ import searchengine.Repositories.LemmaRepository;
 import searchengine.Repositories.PageRepository;
 import searchengine.Repositories.SiteRepository;
 import searchengine.model.Site;
-import searchengine.config.SitesList;
 import searchengine.dto.statistics.DetailedStatisticsItem;
 import searchengine.dto.statistics.StatisticsData;
 import searchengine.dto.statistics.StatisticsResponse;
 import searchengine.dto.statistics.TotalStatistics;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 @Service
 @RequiredArgsConstructor
 public class StatisticsServiceImpl implements StatisticsService {
     @Autowired
-    private IndexingServiceImpl index;
     private final SiteRepository siteRepository;
 
     private final PageRepository pageRepository;
 
-    private final SitesList sites;
     private final LemmaRepository lemmaRepository;
 
     @Override
@@ -48,7 +46,7 @@ public class StatisticsServiceImpl implements StatisticsService {
             item.setLemmas(lemmas);
             item.setStatus(String.valueOf(site.getStatus()));
             item.setError(site.getLastError());
-            item.setStatusTime(System.currentTimeMillis());
+            item.setStatusTime(ZonedDateTime.of(site.getStatusTime(), ZoneId.systemDefault()).toInstant().toEpochMilli());
             total.setPages(total.getPages() + pages);
             total.setLemmas(total.getLemmas() + lemmas);
             detailed.add(item);
